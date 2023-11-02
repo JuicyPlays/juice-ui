611ef4d8fb0f8624d8755dbb2fad2a350f132467
Pankeel Shah <pankeelshahs@gmail.com>
2023-11-01 21:31:45 -0400

    pass in min correlation score

--- src/pages/Correlation.js
+++ src/pages/Correlation.js
@@ -45,6 +45,7 @@ const Correlation = () => {
     const queryParams = {
       parlayBooks: "PRIZEPICKS,UNDERDOG",
       sports: sports.length === 0 ? "CSGO,VAL" : sports.join(","),
+      minCorrelationScore: 1.5 // TODO: Pass in as input on UI
     };
 
     try {
